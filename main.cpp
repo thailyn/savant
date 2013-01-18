c401823dfd34a88b377ad18c4e7eeedb385c5ee4
Charles Macanka <cmacanka@gmail.com>
2013-01-17 19:19:02 -0500

    Load sample xml file and test if the load worked.

--- main.cpp
+++ main.cpp
@@ -3,5 +3,17 @@
 
 int main()
 {
+  pugi::xml_document doc;
+  pugi::xml_parse_result result = doc.load("library.xml");
+
+  if (result)
+  {
+    std::cout << "Result is good!" << std::endl;
+  }
+  else
+  {
+    std::cout << "Result is bad." << std::endl;
+  }
+
   return 0;
 }
