04eafd4590f9d367d8657801ecabfc7dcb130f67
Charles Macanka <cmacanka@gmail.com>
2013-01-18 21:47:02 -0500

    For each song, print out the title and artist.
    When looping through all songs in the library, loop through all "key" child
    elements.  For "Name" and "Artist" elements, get the next sibling, which is
    the value of that key.  Print these values out when they are found, using the
    assumption that Name keys appear before "Artist" keys.  Also include the
    number of the song.

--- main.cpp
+++ main.cpp
@@ -22,7 +22,30 @@ int main()
   for (pugi::xml_node song = songs.child("dict"); song; song = song.next_sibling("dict"))
   {
     i++;
-    std::cout << i << ": Song: " << song.child_value("key") << std::endl;
+    std::cout << i << ": Song: ";
+    std::string title = "";
+    std::string artist = "";
+    for (pugi::xml_node key = song.child("key"); key; key = key.next_sibling("key"))
+    {
+      //std::cout << "Song: ";
+      std::string keyText = key.child_value();
+      if (strcmp(key.child_value(), "Name") == 0)
+      {
+        pugi::xml_node value = key.next_sibling("string");
+        title = value.child_value();
+        //std::cout << value.text() << " - ";
+        std::cout << value.child_value() << " - ";
+      }
+      else if (strcmp(key.child_value(), "Artist") == 0)
+      {
+        pugi::xml_node value = key.next_sibling("string");
+        artist = value.child_value();
+        //std::cout << value.text();
+        std::cout << value.child_value();
+      }
+    }
+    std::cout << std::endl;
+    //std::cout << "*Song: " << title << " - " << artist << std::endl;
   }
 
   return 0;
