29e83ce0b96264944a282eb304bff3b36c9ec66a
Charles Macanka <cmacanka@gmail.com>
2013-01-20 00:19:20 -0500

    Statically link required gcc and c++ libraries.
    Machines that do not have gcc or a C++ environment set up will not have some
    necessary libraries.  For now, statically link them so this application can be
    easily distributed to other people.  Later (e.g., when I create a makefile),
    provide some configuration options for the build to determine if these
    libraries should be statically linked.

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ -Wall -std=c++0x -Ilib lib/pugixml/pugixml.cpp song.cpp main.cpp
+g++ -Wall -static-libgcc -static-libstdc++ -std=c++0x -Ilib lib/pugixml/pugixml.cpp song.cpp main.cpp -o savant.exe
