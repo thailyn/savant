9b23ffac8ea98d9b6dd06e90a76f492ed67b5b29
Charles Macanka <cmacanka@gmail.com>
2013-01-20 00:49:44 -0500

    Build playlist class when building.

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ -Wall -static-libgcc -static-libstdc++ -std=c++0x -Ilib lib/pugixml/pugixml.cpp song.cpp main.cpp -o savant.exe
+g++ -Wall -static-libgcc -static-libstdc++ -std=c++0x -Ilib lib/pugixml/pugixml.cpp song.cpp playlist.cpp main.cpp -o savant.exe
