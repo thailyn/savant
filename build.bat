c6333d67dfc52c235f1b802b053da5771bf09a42
Charles Macanka <cmacanka@gmail.com>
2013-01-21 12:17:23 -0500

    Referenced the boost program_options dll when building.
    This version of the file is very messy, as I was trying to figure out how to
    include a boost library, specifically one needed to be compiled.  The final
    version does work, but does not use a compiler switch (such as I, L, or l) to
    reference where the dll is.  At a later point I will revisit this and figure
    out how to set this up better.
    
    Also, the boost libraries and references will not (at this point, at least) be
    added to the repository, as they are many in number and size.  I need to look
    into how to use the bcp program to pull out just the files I need to compile
    this program.

--- build.bat
+++ build.bat
@@ -1,1 +1,6 @@
-g++ -Wall -static-libgcc -static-libstdc++ -std=c++0x -Ilib lib/pugixml/pugixml.cpp song.cpp playlist.cpp main.cpp -o savant.exe
+rem c:/MinGW/bin/g++ -Wall -Bstatic -static-libgcc -static-libstdc++ -std=c++0x -Ilib -Ltemp/boost_1_52_0/bin.v2/libs/program_options/build/gcc-mingw-4.7.2/debug/link-static/threading-multi -Itemp/boost_1_52_0 -Itemp/boost_1_52_0/libs -Ltemp/boost_1_52_0/libs -Ltemp/boost_1_52_0/stage/lib -Itemp/boost_1_52_0/stage/lib -lboost_program_options-mgw47-mt-d-1_52 lib/pugixml/pugixml.cpp temp/boost_1_52_0/stage/lib/libboost_program_options-mgw47-mt-d-1_52.dll song.cpp playlist.cpp main.cpp -o savant.exe
+rem temp/boost_1_52_0/bin.v2/libs/program_options/build/gcc-mingw-4.7.2/debug/link-static/threading-multi/libboost_program_options-mgw47-mt-d-1_52.a
+rem -lboost_program_options-mgw47-mt-1_52.a
+
+rem -Itemp/boost_1_52_0/stage/lib -Lboost_program_options-mgw47-mt-d-1_52
+g++ -Wall -static-libgcc -static-libstdc++ -std=c++0x -Ilib lib/boost/lib/libboost_program_options-mgw47-mt-d-1_52.dll lib/pugixml/pugixml.cpp song.cpp playlist.cpp main.cpp -o savant.exe
