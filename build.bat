db734bec1aa6571675424352ae366edf4dadc2e3
Charles Macanka <cmacanka@gmail.com>
2013-01-21 19:53:19 -0500

    Do not compile gcc and c++ libraries dynamically.
    The boost program_options library appears to not have the gcc and c++
    libraries statically linked, so the resulting executable needs these in the
    same directory with it.  I have not yet found out how to compile those to
    statically.  Since these need to be in the output directory anyway, now I have
    switched compiling savant.exe to dyanically reference them.  As a result of
    all these, these two libraries need to be in the output directory with the
    exe.

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ -Wall -static-libgcc -static-libstdc++ -std=c++0x -Ilib lib/boost/lib/libboost_program_options-mgw47-mt-d-1_52.dll lib/pugixml/pugixml.cpp song.cpp playlist.cpp main.cpp -o savant.exe
+g++ -Wall -std=c++0x -Ilib lib/boost/lib/libboost_program_options-mgw47-mt-d-1_52.dll lib/pugixml/pugixml.cpp song.cpp playlist.cpp main.cpp -o savant.exe
