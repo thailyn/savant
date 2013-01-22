ca188bb4198ba774a060e74c7067185c48540fc9
Charles Macanka <cmacanka@gmail.com>
2013-01-21 20:45:46 -0500

    Added additional parameters to basic bat file.
    Added parameter --unrated-rating, with value 0, and parameter -n, with value
    25, to the command line.  These both are the default values.  Also added
    comments describing other parameters and allowed values for --unrated-rating.

--- savant.bat
+++ savant.bat
@@ -1,1 +1,3 @@
-savant.exe --input=library.xml --playlist=Music --rating-weight=iTunes --output=playlist.m3u
+rem Additional option: --skip-unrated
+rem --unrated-rating value must be non-negative ([0, 100] is meaningful)
+savant.exe --input=library.xml --playlist=Music --rating-weight=iTunes --unrated-rating=0 --output=playlist.m3u -n 25
